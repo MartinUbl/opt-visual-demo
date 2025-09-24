e89a08eac013e0ea353b62bff399294fb30d1868
MartinUbl <martinubl@seznam.cz>
2025-09-24 19:01:27 +0200

    Modified web build to store emscripten SDK path and restore it if needed

--- build_web.sh
+++ build_web.sh
@@ -7,9 +7,21 @@
 # 4) ./emsdk activate latest
 # 5) source ./emsdk_env.sh
 
+if [ ! $(which emcmake) ]; then
+	if [ -f .emsdk ]; then
+		EMSDK_LOCATION=$(cat .emsdk)
+		if [ -f $EMSDK_LOCATION ]; then
+			echo "Activating emscripten SDK from $EMSDK_LOCATION"
+			source $EMSDK_LOCATION
+		fi
+	fi
+fi
+
 if [ $(which emcmake) ] && [ $(which emcc) ]; then
 	echo "Found emcmake: $(which emcmake)";
 	echo "Found emcc: $(which emcc)";
+	
+	echo $(which emsdk_env.sh) > .emsdk
 else
 	echo "No emcmake and emcc found! Make sure you activated the emsdk environment by calling: source emsdk_env.sh";
 	exit 1;
@@ -17,5 +29,5 @@ fi
 
 mkdir -p embuild
 cd embuild
-emcmake cmake .. -DPLATFORM=Web
+emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release
 cmake --build . --parallel
